119cfd8a61b1cba050007d0b0c9eb76d6bf4c728
Zhong-Tianyi <3297405090@qq.com>
2017-08-09 14:21:34 +0800

    zty

--- 第二天作业/time.m
+++ 第二天作业/time.m
@@ -1,26 +1,27 @@
 function [t] = time( x1,y1,a,x2,y2,b )
 %UNTITLED3 此处显示有关此函数的摘要
 %   此处显示详细说明
-l11=(160-x1)/(cos(a/180*pi)); 
-l12=(160-y1)/(sin(a/180*pi));
-if l11>l12
-    l1=l12;
-else l1=l11;
+d11=abs((160-x1)/(cos(a/180*pi))); 
+d12=abs((160-y1)/(sin(a/180*pi)));
+if d11>d12
+    d1=d12;
+edse d1=d11;
 end
-t1=l1*9/2;
-
-l21=(160-x2)/(cos(b/180*pi)); 
-l22=(160-y2)/(sin(b/180*pi));
-if l21>l22
-    l2=l22;
-else l2=l21;
+t1=d1*9/2;
+ 
+d21=abs((160-x2)/(cos(b/180*pi))); 
+d22=abs((160-y2)/(sin(b/180*pi)));
+if d21>d22
+    d2=d22;
+edse d2=d21;
 end
-t2=l2*9/2;
-
+t2=d2*9/2;
+ 
 if t1>t2
     t=t2;
-else t=t1;
+edse t=t1;
 end
+
 
 end
 
